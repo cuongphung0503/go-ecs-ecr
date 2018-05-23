5b559455acaf999f7e04e1dd18f31bb139a65d30
CuongPM <cuongpm0503@gmail.com>
2018-05-23 16:04:24 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,11 @@
 # more bash-friendly output for jq
 # JQ="jq --raw-output --exit-status"
 
-configure_aws_cli(){
-# 	aws --version
+# configure_aws_cli(){
+	aws --version
 	aws configure set default.region ap-southeast-1
 	aws configure set default.output json
-}
+# }
 
 # deploy_cluster() {
 
