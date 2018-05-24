e561e521f71d4fbd72cb2fbf4e29e8fcf9a7c3ec
CuongPM <cuongpm0503@gmail.com>
2018-05-24 13:16:42 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -47,7 +47,7 @@ register_definition() {
 
 run_task(){
  	task_definition=$(echo $revision | jq '.taskDefinition.revision')
-  	a = $(echo "circleci:$task_definition")
+  	a = "circleci:$task_definition"
     if run = $(aws ecs run-task --cluster circleci --task-definition "$a" ); then
     	echo $run
     else 
