d0ed8ad880128dcc63d3bb80c38719eeee3bbcf6
cuongpm6905 <cuongpm6905@co-well.com.vn>
2018-05-24 09:54:43 +0700

    ok

--- deploy.sh
+++ deploy.sh
@@ -37,7 +37,7 @@ make_task_def(){
 
 register_definition() {
 
-    if revision=$(aws ecs register-task-definition --container-definitions "$task_def" --family $family | $JQ '.taskDefinition.taskDefinitionArn'); then
+    if revision=$(aws ecs register-task-definition --container-definitions "$task_def"); then
         echo "Revision: $revision"
     else
         echo "Failed to register task definitions"
