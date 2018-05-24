31f608abdb789547c29fbb500a9a938dc2936032
CuongPM <cuongpm0503@gmail.com>
2018-05-24 11:23:30 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -38,7 +38,7 @@ make_task_def(){
 register_definition() {
 
     if revision=$(aws ecs register-task-definition --container-definitions "$task_def" --family "circleci"); then
-        echo "Revision: $revision.taskDefinition.revision"
+        echo "Revision: {$revision.taskDefinition.revision}"
     else
         echo "Failed to register task definitions"
         return 1
